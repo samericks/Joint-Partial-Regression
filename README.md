33112a80e5e4f9ba94f4f35515272ce6cf8aad00
Samuel Erickson <samuelea@kth.se>
2025-05-21 14:13:21 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,14 +1,64 @@
 # Joint Partial Regression
 
+`graph-jpr` is a Python package written in Rust that implements joint partial regression and Huber lasso regression.
+
 ## Installation
 
 To install this package, execute
-```
+
+```python
 pip install graph-jpr
 ```
 
-Import 'JointPartialRegression' and 'SparseHuber' by adding
-```
+Import `JointPartialRegression` and `SparseHuber` by adding
+
+```python
 from graph_jpr import JointPartialRegression, SparseHuber
 ```
+
 to the top of your Python code.
+
+## Usage and features
+
+To fit a model, instantiate a model object and call the `fit` method, e.g.,
+
+```python
+import numpy as np
+from graph_jpr import JointPartialRegression
+
+X = np.random.normal(size=(100, 10))
+model = JointPartialRegression()
+precision = model.fit(X, lambdas=0.1)
+```
+
+The `fit` method assumes the rows of the data matrix `X` correspond to the samples. 
+
+`JointPartialRegression` and `SparseHuber`supports quadratic and Huber loss, which can be specified when instantiating the object with either `loss=l2` (default) or `loss='huber'`. When using Huber loss, the threshold parameter is specified by the `rho` parameter when calling `fit`. The `fit` method also supports regularization parameter selection via $K$-fold cross-validation, Aikake information criterion (AIC) or Bayesian information criterion (BIC) (see examples below). 
+
+```python
+precision = model.fit(X, n_lambdas=50, cv_folds=10)
+precision = model.fit(X, n_lambdas=50, criterion='AIC')
+precision = model.fit(X, n_lambdas=50, criterion='BIC')
+```
+
+If `n_lambdas` is specified, then the model selects the parameter range automatically, but it is possible to pre-specify the parameter grid by specifying `lambdas` when calling `fit` with either CV or AIC/BIC.
+
+## Technical details
+
+For fitting, the joint partial regression model uses the PD3O algorithm ([Yin, 2018](https://link.springer.com/article/10.1007/s10915-018-0680-3)), while the Huber lasso regression model uses an implementation FISTA ([Beck and Teboulle, 2009](https://epubs.siam.org/doi/10.1137/080716542)). The Rust crate [ndarray-linalg](https://docs.rs/ndarray-linalg/latest/ndarray_linalg/), which leverages [LAPACK](https://www.netlib.org/lapack/) routines, is used for all linear algebra operations. 
+
+## Citing `graph-jpr`
+
+If you use `graph-jpr`, please cite the following paper:
+
+```
+@misc{graph-jpr,
+ title={Inverse Covariance and Partial Correlation Matrix Estimation via Joint Partial Regression}, 
+ author={Samuel Erickson and Tobias Rydén},
+ year={2025},
+ eprint={2502.08414},
+ archivePrefix={arXiv},
+ primaryClass={stat.ML},
+ url={https://arxiv.org/abs/2502.08414}, 
+}
+```
--- graph_jpr/__init__.py
+++ graph_jpr/__init__.py
@@ -1,58 +1,62 @@
-__version__ = '0.1.0'
-__author__ = 'Samuel Erickson Andersson'
+__version__ = "0.1.0"
+__author__ = "Samuel Erickson Andersson"
 
 from .graph_jpr import fit_sparse_huber, fit_jpr
 
-__all__ = [
-    'JointPartialRegression', 
-    'SparseHuber', 
-    'fit_sparse_huber', 
-    'fit_jpr'
-]
+__all__ = ["JointPartialRegression", "SparseHuber", "fit_sparse_huber", "fit_jpr"]
 
 import numpy as np
 from typing import Union, Iterable
+
 
 class Estimator:
     def __init__(self):
         pass
 
     def _validate_params(self, X, lambdas, rho, n_lambdas, cv_folds, criterion):
-        if self.mode == 'huber' and rho <= 0.0:
-            raise ValueError('rho must be greater than 0.0 when using Huber loss')
+        if self.mode == "huber" and rho <= 0.0:
+            raise ValueError("rho must be greater than 0.0 when using Huber loss")
 
         if np.any(np.array(lambdas) < 0.0):
-            raise ValueError('lambdas must be non-negative')
-                
+            raise ValueError("lambdas must be non-negative")
+
         if len(lambdas) > 0 and n_lambdas > 0:
-            raise ValueError('Cannot pre-specify lambdas and use n_lambdas')
-        
-        use_model_selection = cv_folds > 0 or criterion == 'AIC' or criterion == 'BIC'
+            raise ValueError("Cannot pre-specify lambdas and use n_lambdas")
+
+        use_model_selection = cv_folds > 0 or criterion == "AIC" or criterion == "BIC"
 
         if use_model_selection:
-            if cv_folds > 0 and criterion != '':
-                raise ValueError('Cannot use cross-validation and AIC/BIC at the same time')
+            if cv_folds > 0 and criterion != "":
+                raise ValueError(
+                    "Cannot use cross-validation and AIC/BIC at the same time"
+                )
             elif len(lambdas) > 0 and n_lambdas > 0:
-                raise ValueError('Cannot pre-specify lambdas and specify n_lambdas')
+                raise ValueError("Cannot pre-specify lambdas and specify n_lambdas")
             elif len(lambdas) == 0 and n_lambdas == 0:
-                raise ValueError('Must specify lambdas or n_lambdas when using CV or AIC/BIC')
-            
-            if cv_folds < 2 and criterion == '':
-                raise ValueError('Number of folds must be greater than 1')
+                raise ValueError(
+                    "Must specify lambdas or n_lambdas when using CV or AIC/BIC"
+                )
+
+            if cv_folds < 2 and criterion == "":
+                raise ValueError("Number of folds must be greater than 1")
             elif cv_folds > X.shape[0]:
-                raise ValueError('Number of folds cannot exceed the number of samples')
+                raise ValueError("Number of folds cannot exceed the number of samples")
         else:
             if len(lambdas) != X.shape[1]:
-                raise ValueError('lambdas must be scalar or the same length as the number of features when not using CV or AIC/BIC')
-            
+                raise ValueError(
+                    "lambdas must be scalar or the same length as the number of features when not using CV or AIC/BIC"
+                )
+
             if n_lambdas > 0:
-                raise ValueError('Cannot specify n_lambdas when not using CV or AIC/BIC')
-        
+                raise ValueError(
+                    "Cannot specify n_lambdas when not using CV or AIC/BIC"
+                )
+
 
 class JointPartialRegression(Estimator):
     """
     Joint Partial Regression (JPR) for estimating a sparse precision matrix.
-    
+
     Parameters
     ----------
     mode : str, default='l2'
@@ -63,7 +67,7 @@ class JointPartialRegression(Estimator):
         Maximum number of iterations.
     tol : float, default=1e-6
         Tolerance for convergence.
-        
+
     Attributes
     ----------
     precision_ : np.array
@@ -76,7 +80,7 @@ class JointPartialRegression(Estimator):
         The selected regularization parameters.
     lambdas_grid_ : np.array
         The grid of regularization parameters used for model selection.
-        
+
     Example
     --------
     >>> import numpy as np
@@ -85,8 +89,15 @@ class JointPartialRegression(Estimator):
     >>> model = JointPartialRegression()
     >>> precision = model.fit(X, lambdas=0.1)
     """
-    def __init__(self, mode: str='l2', fit_intercept: bool=True, max_iter: int=100, tol: float=1e-6):
-        if mode not in ['l2', 'huber']:
+
+    def __init__(
+        self,
+        mode: str = "l2",
+        fit_intercept: bool = True,
+        max_iter: int = 100,
+        tol: float = 1e-6,
+    ):
+        if mode not in ["l2", "huber"]:
             raise ValueError('mode must be either "l2" or "huber"')
         self.mode = mode
         self.fit_intercept = fit_intercept
@@ -97,10 +108,18 @@ class JointPartialRegression(Estimator):
         self.selected_lambdas_ = None
         self.lambdas_grid_ = None
 
-    def fit(self, X: np.array, lambdas: Union[float, Iterable[float]]=[], rho: float=0.0, n_lambdas: int=0, cv_folds: int=0, criterion: str=''):
+    def fit(
+        self,
+        X: np.array,
+        lambdas: Union[float, Iterable[float]] = [],
+        rho: float = 0.0,
+        n_lambdas: int = 0,
+        cv_folds: int = 0,
+        criterion: str = "",
+    ):
         """
         Fit the model to the data.
-        
+
         Parameters
         ----------
         X : np.array
@@ -120,7 +139,7 @@ class JointPartialRegression(Estimator):
             The criterion for model selection. Available options are Aikaike
             information criterion ('AIC'), and Bayesian information criterion
             ('BIC').
-            
+
         Returns
         -------
         precision_ : np.array
@@ -130,19 +149,35 @@ class JointPartialRegression(Estimator):
             lambdas = [lambdas] * X.shape[1]
         else:
             lambdas = list(lambdas)
-        
+
         self._validate_params(X, lambdas, rho, n_lambdas, cv_folds, criterion)
-        
-        self.precision_, self.intercepts_, self.selected_lambdas_, self.lambdas_grid_ = fit_jpr(X, lambdas, rho, self.fit_intercept, n_lambdas, cv_folds, criterion, self.max_iter, self.tol)
+
+        (
+            self.precision_,
+            self.intercepts_,
+            self.selected_lambdas_,
+            self.lambdas_grid_,
+        ) = fit_jpr(
+            X,
+            lambdas,
+            rho,
+            self.fit_intercept,
+            n_lambdas,
+            cv_folds,
+            criterion,
+            self.max_iter,
+            self.tol,
+        )
         T = 1 / np.diag(self.precision_) ** 0.5
         self.partial_corr_ = -np.diag(T) @ self.precision_ @ np.diag(T)
 
         return self.precision_
-    
+
+
 class SparseHuber(Estimator):
     """
     l1-regularized Huber regression.
-    
+
     Parameters
     ----------
     fit_intercept : bool, default=True
@@ -151,7 +186,7 @@ class SparseHuber(Estimator):
         Maximum number of iterations.
     tol : float, default=1e-6
         Tolerance for convergence.
-        
+
     Attributes
     ----------
     coef_ : np.array
@@ -160,7 +195,7 @@ class SparseHuber(Estimator):
         The estimated intercept.
     selected_lambda_ : float
         The selected regularization parameter.
-        
+
     Example
     --------
     >>> import numpy as np
@@ -168,20 +203,34 @@ class SparseHuber(Estimator):
     >>> X = np.random.normal(size=(100, 10))
     >>> y = X @ np.random.normal(size=10) + np.random.normal(size=100)
     >>> model = SparseHuber()
-    >>> coef, intercepts = model.fit(X, y, lambdas=0.1, rho=1.0)    
+    >>> coef, intercepts = model.fit(X, y, lambdas=0.1, rho=1.0)
     """
-    def __init__(self, fit_intercept: bool=True, max_iter: int=100, tol: float=1e-6):
+
+    def __init__(
+        self, fit_intercept: bool = True, max_iter: int = 100, tol: float = 1e-6
+    ):
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
         self.tol = tol
         self.coef_ = None
         self.intercept_ = None
         self.selected_lambda_ = None
-                
-    def fit(self, X: np.array, y: np.array, lambdas: Union[float, Iterable[float]], rho: float, n_lambdas: int=0, cv_folds: int=0, criterion: str='', theta_start=None, intercept_start=None):
+
+    def fit(
+        self,
+        X: np.array,
+        y: np.array,
+        lambdas: Union[float, Iterable[float]],
+        rho: float,
+        n_lambdas: int = 0,
+        cv_folds: int = 0,
+        criterion: str = "",
+        theta_start=None,
+        intercept_start=None,
+    ):
         """
         Fit the model to the data.
-        
+
         Parameters
         ----------
         X : np.array
@@ -217,14 +266,29 @@ class SparseHuber(Estimator):
 
         if theta_start is None:
             theta_start = np.zeros(X.shape[1])
-        
+
         if intercept_start is None:
             intercept_start = 0.0
 
         self._validate_params(X, lambdas, rho, cv_folds, criterion)
 
         alpha = 1 / np.linalg.norm(X, ord=2) ** 2
-        self.coef_, self.intercept_, self.selected_lambda_, self.lambda_grid = fit_sparse_huber(X, y, lambdas, rho, self.fit_intercept, n_lambdas, cv_folds, criterion, alpha, self.max_iter, self.tol, theta_start=theta_start)
+        self.coef_, self.intercept_, self.selected_lambda_, self.lambda_grid = (
+            fit_sparse_huber(
+                X,
+                y,
+                lambdas,
+                rho,
+                self.fit_intercept,
+                n_lambdas,
+                cv_folds,
+                criterion,
+                alpha,
+                self.max_iter,
+                self.tol,
+                theta_start=theta_start,
+            )
+        )
         return self.coef_, self.intercept_
 
     def predict(self, X: np.array):
